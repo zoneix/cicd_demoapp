d86cc03a102c754384be7f9a047d0b9879d5060a
Hank Preston <hank.preston@gmail.com>
2016-07-01 09:46:40 -0400

    Added hello/universe to the application

--- demoapp.py
+++ demoapp.py
@@ -11,20 +11,14 @@ class HelloWorld(Resource):
         text = "Hello World!"
         return text
 
-class HelloClass(Resource):
-    def get(self):
-        text = "Hello Class!"
-        return text
-
-class HelloTest(Resource):
-    def get(self):
-        text = "Hello Test!"
-        return text
-
-
 api.add_resource(HelloWorld, '/hello/world')
-api.add_resource(HelloClass, '/hello/class')
-api.add_resource(HelloTest, '/hello/test')
+
+class HelloUniverse(Resource):
+    def get(self):
+        text = "Hello Universe!"
+        return text
+
+api.add_resource(HelloUniverse, '/hello/universe')
 
 if __name__ == '__main__':
     # Runn Flask
