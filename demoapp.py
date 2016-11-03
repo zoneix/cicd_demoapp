faf404ede539be7e4dca287ee0a14d0fe8f6819f
Harish Chawla <zoneix@gmail.com>
2016-11-03 15:41:21 -0400

    added hello jimmy boy

--- demoapp.py
+++ demoapp.py
@@ -13,6 +13,13 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/hello/world')
 
+class HelloJimmy(Resource):
+    def get(self):
+        text = "Hello Jimmy!"
+        return text
+
+api.add_resource(HelloJimmy, '/hello/jimmy')
+
 if __name__ == '__main__':
     # Runn Flask
     app.run(debug=True, host='0.0.0.0', port=int("5000"))
